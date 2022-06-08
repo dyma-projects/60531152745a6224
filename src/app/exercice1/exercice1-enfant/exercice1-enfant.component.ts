1c1939b7d0a1b6c41247cefd78d194881ed01bb9
marie durieux <marie.durieux@nrb.be>
2022-06-08 16:47:30 +0200

    ex 1

--- src/app/exercice1/exercice1-enfant/exercice1-enfant.component.ts
+++ src/app/exercice1/exercice1-enfant/exercice1-enfant.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-exercice1-enfant',
@@ -7,9 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Exercice1EnfantComponent implements OnInit {
 
+  @Input() public result : number = 0;
+  public resultTotal : number;
+  @Output() IncrementNumber : EventEmitter<number> = new EventEmitter();
+  @Output() DecrementNumber : EventEmitter<number> = new EventEmitter();
+
+
+
   constructor() { }
 
   ngOnInit() {
   }
+
+  public increment(reference: HTMLInputElement) {
+    this.IncrementNumber.emit(this.result++);
+  }
+
+  public decrement(reference: HTMLInputElement) {
+    this.DecrementNumber.emit(this.result--);
+  }
+
+
+
 
 }
